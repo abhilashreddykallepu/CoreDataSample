90d862959a836f179964561d88b36eff21c83801
abhiii <HadiHatinoglu@Hadis-iMac.local>
2013-11-25 12:42:21 +0530

    Initial Commit

---
+++ CoreDataSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataSample
+//
+//  Created by Hadi Hatinoglu on 25/11/13.
+//  Copyright (c) 2013 Baraansoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
